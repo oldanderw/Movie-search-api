bcf79265ad8206be8d0b4d9d3b54ad2a19d228b3
David parker <adparker@fullsail.edu>
2017-06-04 23:47:07 -0400

    first mile stone

--- js/main.js
+++ js/main.js
@@ -14,25 +14,26 @@ form.addEventListener('submit', event => {
     if (json.totalItems == 0){
       text += "No Results Found"
     }else{text += `<div class="titles">`
-      // variables declared before loop so they aren't being re-made everytime the loop go over it but instead being over writen
-      let image, title, description;
+      //variables declared before loop so they aren't being re-made everytime the loop go over it but instead being over writen
+      let image, title; //description;
       for (var i = 0; i < json.items.length; i++) {
         console.log(`i got to the ${i+1} item`)
         title = json.items[i].volumeInfo.title;
-        //title.substring(0,30);
-        description = json.items[i].volumeInfo.description;
-        //description.substring(0,150);
+        title.substring(0,30);
+        let description = json.items[i].volumeInfo.description;
+        description.toString().substring(0,3);
+        console.log(typeof description)
         //if thumbnail exist then is the thumbnail if not then use the local image in the Assets folder
-         if(json.items[i].volumeInfo.imageLinks.thumbnail == null){
-           image = json.items[i].volumeInfo.imageLinks.thumbnail;
-         }else{
-           image = "../Assets/No_Image_Available.gif";
-           console.log("i tried to work");
-         }
-        text += `<article>
-        <img src="${image}" alt="" />
-          <a href="${json.items[i].selfLink}">${title}</a>
-          <a target="_blank" href="${json.items[i].selfLink}">${description}</a>
+       if(json.items[i].volumeInfo.imageLinks.thumbnail != null){
+         image = json.items[i].volumeInfo.imageLinks.thumbnail;
+       }else{
+         image = "../Assets/No_Image_Available.gif";
+         console.log("i tried to work");
+       }
+       text += `<article>
+      <img src="${image}" alt="" />
+        <a href="${json.items[i].selfLink}">${title}</a>
+        <a target="_blank" href="${json.items[i].selfLink}">${description}</a>
       </article>`
     }
   }
